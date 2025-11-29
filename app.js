73415c0578735c85bd8a6461991de6a5b0eadf7e
ZahraDH28 <diahandokozahra29@gmail.com>
2025-11-29 23:25:09 +0700

    a

--- app.js
+++ app.js
@@ -11,15 +11,9 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Redirect untuk semua route ke index.html (untuk SPA)
-app.get("*", (req, res) => {
+// Catch-all route untuk semua path lainnya
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-// Error handling
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Terjadi error pada server");
 });
 
 app.listen(3000, () => console.log("Frontend running at https://vercel.com/zahradh28s-projects/frontend-titik-banjir"));
